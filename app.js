2f2364ddf8ab037e3de6440a9dd9a942ccc6b6a5
brianfajardo <bmf5@hotmail.com>
2017-03-05 11:13:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+    .options({
+        address: {
+            demand: true,
+            alias: 'a',
+            describe: 'Address to fetch weather for',
+            string: true
+        },
+    })
+    .help()
+    .alias('help', 'h')
+    .argv
+
+const encodedAddress = encodeURIComponent(argv.address)
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true /* accept JSON data back */
+}, (error, response, body) => {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+})
